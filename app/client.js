baf4ab776ca6fe1927db2812cfc5310b573f140e
stopachka <stepan.p@gmail.com>
2016-02-15 13:33:08 -0800

    render react on client

--- app/client.js
+++ app/client.js
@@ -1,1 +1,11 @@
-console.log('hello');
+import routes from './routes';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {match, browserHistory, Router} from 'react-router';
+
+match({history: browserHistory, routes}, (err, redirect, props) => {
+  ReactDOM.render(
+    <Router {...props} />,
+    document.getElementById('react-root')
+  );
+});
--- app/server.js
+++ app/server.js
@@ -13,7 +13,13 @@ app.use('/client.js', (req, res) => {
 
 app.get('/:params?*', (req, res) => {
   match({routes, location: req.url}, (err, redirect, props) => {
-    res.status(200).send(templ(renderToString(<RouterContext {...props} />)));
+    if (err) {
+      res.status(500).send(error.messsage);
+    } else if (redirect) {
+      res.redirect(302, redirect.pathname + redirect.search);
+    } else {
+      res.status(200).send(templ(renderToString(<RouterContext {...props} />)));
+    }
   });
 });
 
@@ -31,7 +37,7 @@ function templ(body) {
         <meta name="viewport" content="width=device-width, initial-scale=1">
       </head>
       <body>
-        <div class="react-root">${body}</div>
+        <div id="react-root">${body}</div>
         <script src="client.js"></script>
       </body>
     </html>
