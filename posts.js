d7e40ef60313af0e1dd9483d39717936565c8d6e
stopachka <stepan.p@gmail.com>
2016-06-28 16:35:33 -0700

    sm

--- posts.js
+++ posts.js
@@ -1316,7 +1316,7 @@ const POSTS = {
     "updatedAt": 1466227052106,
   },
   "234": {
-    "content": "As we introspect, the world separates from us. We start asking, what do we want? We almost dehumanize those around us. But if we open our eyes, we start see expressions on people's faces. We start to sense the emotions they feel, when they wonder what we're thinking. At that moment, it's almost silly. Wow, how together we really are, how much of the human experience we really share."
+    "content": "As we introspect, the world separates from us. We start asking, what do we want? We almost dehumanize those around us. But if we open our eyes, we start see expressions on people's faces. We start to sense the emotions they feel, when they wonder what we're thinking. At that moment, it's almost silly. Wow, how together we really are, how much of the human experience we really share.",
     "id": "234",
     "title": "On the overwhelming warmth and beauty in humanity",
     "createdAt": 1467087970941,
