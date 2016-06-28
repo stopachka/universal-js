b8b0fcef1feadb522cab3813feeda4dbb1c474e7
stopachka <stepan.p@gmail.com>
2016-06-27 21:26:24 -0700

    humanity

--- posts.js
+++ posts.js
@@ -1315,6 +1315,14 @@ const POSTS = {
     "createdAt": 1466227052106,
     "updatedAt": 1466227052106,
   },
+  "234": {
+    "content": "As we introspect, the world separates from us. We start asking, what do we want? We almost dehumanize those around us. But if we open our eyes, we start see expressions on people's faces. We start to sense the emotions they feel, when they wonder what we're thinking. At that moment, it's almost silly. Wow, how together we really are, how much of the human experience we really share."
+    "id": "234",
+    "title": "On the overwhelming warmth and beauty in humanity",
+    "createdAt": 1467087970941,
+    "updatedAt": 1467087970941,
+  },
+}
 }
 
 export default POSTS;
